de4be7925866fbacf49ff6705b5fd1fdb62e296f
Rocky239 <nicholastwardeski@gmail.com>
2017-03-28 19:54:33 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -10,11 +10,11 @@
 /*URL for the server logo
 Leave it blank to use text instead
 (Put the files in the images folder)*/
-var l_serverImage = "logo.png";
+var l_serverImage = "logo1.png";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = "Revan Gaming";
+var l_serverName = "";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
@@ -100,7 +100,7 @@ var l_musicRandom = true;
 
 /*Music volume
 Choose a value between 0 and 100*/
-var l_musicVolume = 15;
+var l_musicVolume = 10;
 
 /*Enable custom messages?*/
 var l_messagesEnabled = true;
